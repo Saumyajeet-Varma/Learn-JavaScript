e19adda57c6ae929a7ad73a2a9e2505725703da6
Saumyajeet-Varma <saumyajeetv@gmail.com>
2024-12-18 01:27:00 +0530

    slice and splice done

--- 02-Basics/01-Array.js
+++ 02-Basics/01-Array.js
@@ -5,6 +5,8 @@ let anotherArr = new Array(1, 2, 3, 4, 5);
 console.log(myArr) // [0, 1, 2, 3, 4, 5]
 console.log(anotherArr) // [1, 2, 3, 4, 5]
 
+myArr.length; // 6
+
 
 
 // Array Methods
@@ -13,24 +15,27 @@ myArr.push(7); // [0, 1, 2, 3, 4, 5, 6, 7]
 
 myArr.pop(); // [0, 1, 2, 3, 4, 5, 6]
 
+myArr.unshift(9); // [9, 0, 1, 2, 3, 4, 5, 6]
+myArr.shift(); // [0, 1, 2, 3, 4, 5, 6]
+
 myArr.includes(2); // true
 
 myArr.indexOf(4); // 4
-
-myArr.unshift(9); // 8
-myArr.shift(); // 9
 
 
 
 // Join
 const newArr = myArr.join()
 console.log(newArr); // 0,1,2,3,4,5,6
+console.log(typeof newArr) // string
 
 
 
 // slice and splice
-const s1 = myArr.slice(1, 3); // [1, 2]
-const s2 = myArr.splice(1, 3); // [1, 2, 3]
+const s1 = myArr.slice(1, 3); // Range - [1, 3)
+console.log(s1); // [1, 2]
+console.log(myArr); // no change in myArr
 
-console.log(s1);
-console.log(s2);
+const s2 = myArr.splice(1, 3); // Range - [1, 3]
+console.log(s2); // [1, 2, 3]
+console.log(myArr); // s2 is not in myArr anymore
